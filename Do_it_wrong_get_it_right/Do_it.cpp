be1f24fea6f57987aee3f2fc5e3e333fcf3cfd8b
mrhhug <mrhhug@gmail.com>
2013-10-02 12:01:45 -0400

    Do it right complted

---
+++ Do_it_wrong_get_it_right/Do_it.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+#define MIN(a,b) a<b?a:b
+int b;
+int n;
+
+int numerator (int m)
+{
+	return -((b*m*(m-2*n)/(n*n)));
+}
+			
+int factor ()
+{
+	int ans;
+	for (int i =2 ;i<MIN(b,n);i++)
+	{
+		if(b%i==0 && n%i==0)
+		{
+			ans = 	n/i;
+			break;
+		}
+	}
+	return ans;
+}
+
+void takeinput()
+{
+	cin >> b;
+	cin >> n;
+}
+
+int main ()
+{
+	while(true)
+	{
+		takeinput();
+		if (b == 0 && n== 0)
+		{
+			break;
+		}
+		int fac = factor();
+		int denom = 2*n;
+		//cout << denom <<endl;
+		while (denom >= fac)
+		{
+			if (denom != n)
+			{
+			cout << numerator(denom) << "/" << denom << " ";
+			}
+			denom -= fac;
+		}
+		cout << endl;
+	}
+}
