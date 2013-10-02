4b63663d90557cf4bd2adbc1302954d3c6c9a5b1
zackwr <zackscary@gmail.com>
2013-10-02 14:30:29 -0400

    Nonogram changed to cpp, needs more work

--- Nonogram/nonogram.cpp
+++ Nonogram/nonogram.cpp
@@ -1,15 +1,16 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 
 int main()
 {
 	int n, i , j, count;
-	scanf("%d", &n);
+	cin >> n;
 
 	char a[n+1][n+1];
 
 	for(i=0;i<n;i++) {
 		for(j=0;j<n;j++) {
-			scanf("%c", &a[j][i]);
+			cin >> a[i][j];
 		}
 	}
 	for(i=0;i<n;i++) {
@@ -19,11 +20,11 @@ int main()
 				count++;
 			else {
 				if(count > 0) {
-					printf("%d\n", count);
+					cout << count << endl;
 					break;
 				}
 			}
-		printf("%d\n", count);
+			cout << endl;
 		}
 	}
 return 0;
