9b22db42edbb83ac235622ba1f53f98a59e60aff
zackwr <zackscary@gmail.com>
2013-10-02 14:24:08 -0400

    slight modifications, will go back and fix it up.

--- Do_it_wrong_get_it_right/Do_it.cpp
+++ Do_it_wrong_get_it_right/Do_it.cpp
@@ -3,24 +3,25 @@
 using namespace std;
 
 #define MIN(a,b) a<b?a:b
-int b;
-int n;
+int b, n;
 
-int numerator (int m)
+int numerator(int m)
 {
 	return -((b*m*(m-2*n)/(n*n)));
 }
 			
-int factor ()
+int factor()
 {
 	int ans;
 	for (int i =2 ;i<MIN(b,n);i++)
 	{
 		if(b%i==0 && n%i==0)
 		{
-			ans = 	n/i;
+			ans = n/i;
 			break;
 		}
+		else
+			ans = 1;
 	}
 	return ans;
 }
@@ -36,7 +37,7 @@ int main ()
 	while(true)
 	{
 		takeinput();
-		if (b == 0 && n== 0)
+		if (b == 0 && n == 0)
 		{
 			break;
 		}
