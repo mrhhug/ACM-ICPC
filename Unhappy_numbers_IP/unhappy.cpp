7e7a0d5349fe3842156721945702e419cb7b6e5a
mrhhug@gmail.com <mrhhug@gmail.com>
2013-10-04 19:29:34 -0400

    added to_string to Bigint

--- Unhappy_numbers_IP/unhappy.cpp
+++ Unhappy_numbers_IP/unhappy.cpp
@@ -1,3 +1,4 @@
+#include "Bigint.h"
 #include <vector>
 #include <string>
 #include <iostream>
@@ -5,104 +6,32 @@
 
 using namespace std;
 
-vector<string> Vstring;
-string lo;
-string hi;
+string zero = "0";
+Bigint hi;
+Bigint lo;
+Bigint bigzero = zero;
 
-string incrament(string str)
+
+
+string square(string num)
 {
-	char ch = '9';
-	if(str.size()==1)
-	{
-		ch = str.at(0);
-		if(ch!='9')
-		{
-			ch++;
-			str = ch;
-			return str;
-		}
-		return "10";
-	}
-	if(str.at(0)=='9')
-	{
-		str="0"+str;
-	}
-	int count =1;
-	while(ch == '9')
-	{
-		ch = str.at(str.size()-count);
-		count++;
-	}
-	count -=2;
-	if(count != 0)
-	{
-		ch = str.at(str.size()-count-1);
-		ch++;
-		str=str.substr(0,str.size()-count-1);
-		str+=ch;
-		for (int i=0;i<count;i++)
-		{
-			str+='0';
-		}
-	}
-	return str;
+	int n = atoi(num.c_str());
+	n = n*n;
+	//char *intStr = itoa(n);
+	return to_string(n);
 }
-
-string square(char num)
+string pullapartnum(Bigint num)
 {
-	if(num=='0')
-	{
-		return "";
-	}
-	if(num=='1')
-	{
-		return "1";
-	}
-	if(num=='2')
-	{
-		return "4";
-	}
-	if(num=='3')
-	{
-		return "9";
-	}
-	if(num=='4')
-	{
-		return "16";
-	}
-	if(num=='5')
-	{
-		return "25";
-	}
-	if(num=='6')
-	{
-		return "36";
-	}
-	if(num=='7')
-	{
-		return "49";
-	}
-	if(num=='8')
-	{
-		return "64";
-	}
-	if(num=='9')
-	{
-		return "81";
-	}
-}
-
-string pullapartnum(string num)
-{
-	cout << num << endl;
+	//cout << num << endl;
+	string oldstring = num.to_string();
 	string newstring = "";
 	for (int i =0 ;i<num.size();i++)
 	{
-		newstring+=square(num.at(i));
+		newstring+=square(oldstring.at(i));
 	}
 	return newstring;
 }
-
+/*
 int checkhappy (string num)
 {
 	string count ="1";
@@ -118,28 +47,26 @@ int checkhappy (string num)
 	}
 	else return 1;
 }
+*/
 
 void takeinput()
 {
-	cin >> lo;
-	cin >> hi;
+	string num;
+	cin >> num;
+	hi=num;
+	cin >> num;
+	lo=num;
 }
-
-
 
 int main ()
 {
-	cout << pullapartnum("665") << endl;
-	//cout << checkhappy("70") << endl;
-	//cout << incrament("99") << endl;
 	while (false)
 	{
 		takeinput();
-		if (lo == "0" && hi == "0")
+		if (lo == bigzero && hi == bigzero)
 		{
 			break;
 		}
-
 	}
 	//cout << count << endl;
 	//cout << str << endl;
