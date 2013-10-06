aca63ba4047d85c3f9ab7f27b6ebf5a1e1ed298f
mrhhug@gmail.com <mrhhug@gmail.com>
2013-10-06 00:36:35 -0400

    now i have an inefficent algorithm AND race conditions

--- Unhappy_numbers_IP/unhappy.cpp
+++ Unhappy_numbers_IP/unhappy.cpp
@@ -1,89 +1,98 @@
-#include "Bigint.h"
-#include <vector>
+#include <algorithm>
 #include <string>
 #include <iostream>
-#include <stdlib.h>
-
-#define NUM_THREADS 5
+#include <thread>
+#include <sstream>
 
 using namespace std;
 
-string zero = "0";
-string one = "1";
-Bigint hi;
-Bigint lo;
-Bigint bigzero = zero; 
-Bigint bigone = one;
-Bigint maincount;
+unsigned long long maincount,hi,lo;
 
-Bigint square(char num)
+void do_join(thread& t)
+{
+	t.join();
+}
+void join_all(vector<thread>& v)
+{
+	for_each(v.begin(),v.end(),do_join);
+}
+void incramentmaincount()
+{
+	maincount++;
+}
+unsigned long long square(char num)
 {
 	int n = num-'0';
-	n = n*n;
-	Bigint returnval;
-	returnval = to_string(n);
-	return returnval;
+	return n*n;
 }
-Bigint pullapartnum(Bigint num)
+unsigned long long pullapartnum(unsigned long long num)
 {
-	string oldstring = num.to_string();
-	Bigint newstring = zero;
-	for (int i =0 ;i<num.size();i++)
+	stringstream ss;
+	ss << num;
+	string input = ss.str();
+	unsigned long long newstring = 0;
+	for (int i =0 ;i<input.size();i++)
 	{
-		newstring=newstring + square(oldstring.at(i));
+		newstring=newstring + square(input.at(i));
 	}
 	return newstring;
 }
-bool checkhappy (Bigint num)
+void checkhappy (unsigned long long num)
 {
-	string MAXDEPTH = "5";
-	Bigint bigmax = MAXDEPTH;
-	string count ="1";
-	Bigint bigcount = count;
-	//cout << num.to_string() << endl;
-	while (bigone < num)
+	int MAXDEPTH = 25;
+	int count =0;
+	unsigned long long inputnumber = num;
+	while (num > 1)
 	{
-		if (num < bigmax)
+		if (MAXDEPTH < count)
 		{
-			//maincounter = maincounter+bigone;
-			return false;
+			incramentmaincount();
+			return;
 		}
 		num = pullapartnum(num);
-		//cout << num.to_string() << endl;
+		count++;
 	}
-	return true;
+	return;
 }
 void takeinput()
 {
-	string cin0;
-	cin >> cin0;
-	lo=cin0;
-	string cin1;
-	cin >> cin1;
-	hi=cin1;
+	string cinhi,cinlo;
+	cin >> cinlo;
+	cin >> cinhi;
+	hi = strtoull(cinhi.c_str(),NULL, 10);
+	lo = strtoull(cinlo.c_str(),NULL, 10);
 }
 int main ()
 {
-	//string str = "2";
-	//Bigint number = str;
-	//cout << checkhappy(number) << endl;
 	while (true)
 	{
-		maincount = bigzero;
+		int MAXTHREADS = 3;
+		maincount = 0;
 		takeinput();
-		if (hi == bigzero && lo == bigzero)
+		if (hi == 0 && lo == 0)
 		{
 			break;
 		}
-		for ( ; lo<hi+bigone;lo=lo+bigone)
+		int arraycount;
+		while(lo<=hi)
 		{
-			if(!checkhappy(lo))
+			arraycount =1;		
+			thread threadarray[MAXTHREADS];
+			threadarray[0]=thread(checkhappy,lo);
+			lo++;
+			for(int i =1;i<MAXTHREADS && lo<=hi;i++)
 			{
-				maincount = maincount + bigone;
+				threadarray[i]=thread(checkhappy,lo);	
+				lo++;
+				arraycount++;
 			}
+			for (int i =0; i<arraycount;i++)
+			{
+				threadarray[i].join();
+			}
+			
 		}
-		cout << maincount.to_string() << endl;
+		cout << maincount << endl;
 	}
-	//cout << count << endl;
-	//cout << str << endl;
 }
+
