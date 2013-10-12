b810f5c17f95c4a8cffe645c3be011ed20606533
mrhhug <mrhhug@gmail.com>
2013-10-11 20:40:16 -0400

    hailsotne

---
+++ Hailstone/Hailstone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[100000];
+int domath(int n)
+{
+	if(n%2==0)
+	{
+		return n/2;
+	}
+	return n*3+1;
+}
+int main()
+{
+	int imax;
+	//cin >> imax;
+	//for(int i =0;i<=imax;i++)
+	{
+	//	cout << "yes" << endl;
+	}
+	cout << 71/25 <<endl"
+}
