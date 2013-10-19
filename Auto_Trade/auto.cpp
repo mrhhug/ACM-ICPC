44aa9a96525ec15786d97e680331a9ef82aa49dc
zackwr <zackscary@gmail.com>
2013-10-18 20:35:37 -0400

    keep trying

--- Auto_Trade/auto.cpp
+++ Auto_Trade/auto.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 
 int max(int a, int b) { return a>b?a:b; }
-
+int min(int a, int b) { return a<b?a:b; }
+int abs(int a) { return a>0?a:-a; }
+int abs(int a) { return a>0?a:-a; }
 int lcs(char x[], char y[], int m, int n)
 {
 	int l[m+1][n+1];
 
-	for(int i=0;i<=m;i++) {
-		for(int j=0;j<=n;j++) {
-			if(i == 0 || j == 0)
-				l[i][j] = 0;
-			else if(x[i-1] == y[j-1])
-				l[i][j] = l[i-1][j-1] + 1;
-			else
-				l[i][j] = max(l[i-1][j], l[i][j-1]);
+	for(int k=0;k<min(strlen(x),strlen(y);k++) {
+		for(int i=0;i<=m;i++) {
+			for(int j=0;j<=n;j++) {
+				if(i == 0 || j == 0)
+					l[i][j] = 0;
+				else if(x[i-1] == y[j-1])
+					l[i][j] = l[i-1][j-1] + 1;
+				else
+					l[i][j] = max(l[i-1][j], l[i][j-1]);
+			}
 		}
 	}
 return l[m][n];
@@ -29,17 +33,18 @@ return i;
 int main()
 {
 	//char a[5] = "ABAB", b[5] = "ABAB";
-	cout << lcs(a, b, strlen(a), strlen(b)) << endl;
-	while(false) {
-		char s[100000], x[100000], y[100000];
+	//cout << lcs(a, b, strlen(a), strlen(b)) << endl;
+	while(true) {
+		char s[100000];
 		cin >> s;
-		if(s == "*")
+		if(s[0] == '*')
 			return 0;
 		//cout << "Strlen: " << strlen(s) << endl;
 
 		int q;
 		cin >> q;
 		while(q--) {
+			char x[100000], y[100000];
 			int i, j, h;
 			cin >> i >> j;
 			for(int k=i, h=0;k<strlen(s);k++, h++)
