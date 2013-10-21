7f5847df3c6e162710658f51a5054585f29ad340
mrhhug@gmail.com <mrhhug@gmail.com>
2013-10-20 20:30:30 -0400

    admiral lost

--- Unreal_estate/unreal.cpp
+++ Unreal_estate/unreal.cpp
@@ -1,38 +1,53 @@
 #include <iostream>
+#include <vector>
+//#include <bitset>
 
 using namespace std;
+const int matrixsize = 100000;
+//bool matrix [100][100];
+vector< vector<bool> > matrix;
 
-bool matrix [2000][2000];
-
+void creatematrix()
+{
+	for (int i =0;i<matrixsize;i++)
+	{
+		vector<bool> small;
+		for (int j =0;j<matrixsize;j++)
+		{
+			small.push_back(false);
+		}
+		matrix.push_back(small);
+	}
+}
 void clearmatrix()
 {
-	for (int i =0;i<2000;i++)
+	for (int i =0;i<matrixsize;i++)
 	{
-		for (int j =0;j<2000;j++)
+		for (int j =0;j<matrixsize;j++)
 		{
-			matrix [i][j]=false;
+			matrix.at(i).at(j)=false;
 		}
 	}
 }
 void addtomatrix(int x1,int y1,int x2,int y2)
 {
-	x1+=1000;y1+=1000;x2+=1000;y2+=1000;
+	//x1+=1000;y1+=1000;x2+=1000;y2+=1000;
 	for (int i=x1;i<x2;i++)
 	{
 		for (int j=y1;j<y2;j++)
 		{
-			matrix [i][j]=true;
+			matrix.at(i).at(j)=true;
 		}
 	}
 }
 int countmatrix()
 {
 	int count =0;
-	for (int i=0;i<2000;i++)
+	for (int i=0;i<matrixsize;i++)
 	{
-		for (int j=0;j<2000;j++)
+		for (int j=0;j<matrixsize;j++)
 		{
-			if (matrix [i][j]==true)
+			if (matrix.at(i).at(j)==true)
 			{
 				count++;
 			}
@@ -47,16 +62,14 @@ int main()
 	
 	//x1=0;y1=100;x2=300;y2=200;
 	//xx1=100;yy1=0;xx2=200;yy2=300;
-	clearmatrix();
-	addtomatrix(0,0,100,100);
-	addtomatrix(30,30,60,60);
-	cout << countmatrix() << endl;
-	clearmatrix();
-	addtomatrix(0,100,300,200);
-	addtomatrix(100,0,200,300);
-	cout << countmatrix() << endl;
+	creatematrix();
+	cout << "done" ;
+//	addtomatrix(0,0,100,100);
+//	addtomatrix(30,30,60,60);
+//	cout << countmatrix() << endl;
+//	clearmatrix();
+//	addtomatrix(0,100,300,200);
+//	addtomatrix(100,0,200,300);
+//	cout << countmatrix() << endl;
 	return 0;
 }
-
-
-
