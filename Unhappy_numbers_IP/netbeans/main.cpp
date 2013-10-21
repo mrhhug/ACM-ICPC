c8501c4514b24d13c7caefffa3159fad497794b1
mrhhug@gmail.com <mrhhug@gmail.com>
2013-10-21 03:22:32 -0400

    admiral

--- Unhappy_numbers_IP/netbeans/main.cpp
+++ Unhappy_numbers_IP/netbeans/main.cpp
@@ -1,102 +1,162 @@
-#include <vector>
-#include <string>
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
-vector<string> Vstring;
-string word0;
-string word1;
-int num=-1;
-
-void sort ()
+//these are edges, i am keeping track of edges, not nodes.
+struct edge
 {
-	sort(Vstring.begin(),Vstring.end());
-}
-bool takeinput()
+	int source;
+	int dest;
+	int weight;
+	bool visited;
+	vector<edge> children;
+};
+// the only thing is that you have to keep this graph connected, so take all the nodes and sort or something
+class graph
 {
-	Vstring.clear();
-	cin >> num;
-	if(num==0)
+private:
+	int size;
+	edge head;
+	void addVer(edge nd)
 	{
-		return false;
-	}
-	string str;
-	for (int i =0;i<num;i++)
-	{
-		cin >> str;
-		Vstring.push_back(str);
-	}
-	return true;
-}
-void print()
-{
-	for(int i =0;i<Vstring.size();i++)
-	{
-		cout << "\n" << Vstring[i];
-	}
-	cout << "\n" << endl;
-}
-void findwords()
-{
-	if(Vstring.size()==2)
-	{
-		word0=Vstring[0];
-		word1=Vstring[1];
-	}
-	else
-	{
-		word0 = Vstring[Vstring.size()/2-1];
-		word1 = Vstring[Vstring.size()/2];
-	}
-}
-void findletter()
-{
-	//cout << word0 << " " << word1 << endl;
-	cout << word0 << "  " <<word0.size()<< " "<< word1 << "______";
-	if (word0.size()==1==word1.size())
-	{
-        cout << word0;
-    }
-	int i=0;
-	bool printed=false;
-	for ( ; word0.size() > i && word1.size() > i ;i++)
-	{
-		if(word0.compare(i,1,word1,i,1)==0)
+		if(size==0)
 		{
-            printed=true;
-            cout << word0.substr(i,1);
+			head.dest=nd.source;
+			head.children.push_back(nd);
 		}
 		else
 		{
-            break;
-        }
+			vector<edge> spath = shortestHops(head.dest, nd.source);
+			print(spath);
+			if(spath.size()==0)
+			{
+				head.children.push_back(nd);
+			}
+			else
+			{
+				
+				spath.at(spath.size()).children.push_back(nd);
+				//sort(spath.begin(),spath.end());
+			}
+		}
+		//cout <<size<<endl;
+		size++;
 	}
-	//if (false)
-	if(!printed)
+public:
+	//create the head node
+	graph()
 	{
-        char c,d;
-        c=word0[0];
-        d=word1[0];
-        c++;
-        if(c!=d)
-        {
-            //cout<<c;
-        }
+		edge ed;
+		ed.source=-1;
+		ed.dest=-1;
+		ed.weight=-1;
+		ed.visited=false;
+		size=0;
 	}
-	cout << endl;
-
-}
-int main ()
+	//call this to add to graph
+	void add(int source,int dest,int weight)
+	{
+		edge nd;
+		nd.source=source;
+		nd.dest=dest;
+		nd.weight=weight;
+		nd.visited=false;
+		addVer(nd);
+	}
+	//pass something like -2 for a full bfs
+	vector<edge> bfs(int dest)
+	{
+		vector<edge> path;
+		queue<edge> q;
+		edge current;
+		current = head;
+		for(int i=0;i<current.children.size();i++)
+		{
+			q.push(current.children.at(i));
+		}
+		while(q.size()>0)
+		{
+			current = q.front();
+			q.pop();
+			path.push_back(current);
+			for(int i =0;current.children.size();i++)
+			{
+				q.push(current.children.at(i));
+			}
+		}
+		return path;
+	}
+	vector<edge> shortestHops(int source, int dest)
+	{
+		vector<edge> vect = bfs(-2);
+		vector<edge> returnPath;
+		vector<edge> possiblePath;
+		bool beCounting = false;
+		//find shortest path from shource to dest. 
+		for(int i =0;i<vect.size();i++)
+		{
+			//printEdgeVect(vect);
+			if(vect.at(i).source==source)
+			{
+				beCounting=true;
+			}
+			if(vect.at(i).source==dest)
+			{
+				beCounting=false;
+				if(returnPath.size()<possiblePath.size())
+				{
+					returnPath=possiblePath;
+					possiblePath.clear();
+				}
+			}
+			if(beCounting)
+			{
+				possiblePath.push_back(vect.at(i));
+			}
+		}
+		if(returnPath.size()<possiblePath.size())
+		{
+			returnPath=possiblePath;
+			possiblePath.clear();
+		}
+		return returnPath;
+	}
+	void print(vector<edge> vect)
+	{
+		cout << "size =" << vect.size();
+		for(int i =0;i<vect.size();i++)
+		{
+			cout << "source :" << vect.at(i).source << " dest : " << vect.at(i).dest << " " << endl;
+		}
+	}
+	edge returnHead()
+	{
+		return head;
+	}
+};
+int main()
 {
-	string str ="q";
-	//cout << str.size() << endl;
-	while(takeinput())
+	graph gr;
+	gr.add(1,2,35);
+	gr.add(1,3,35);
+	gr.add(1,4,35);
+	gr.add(2,7,35);
+	gr.add(2,6,35);
+	gr.add(2,5,35);
+	gr.add(2,8,35);
+	gr.add(7,8,99);
+	gr.add(4,12,35);
+	gr.add(4,10,15);
+	gr.add(4,11,45);
+	gr.add(3,9,99);
+	vector<edge> path = gr.bfs(-2);
+	cout << "starting loop" << endl;
+	for(int i =0;i<path.size();i++)
 	{
-		sort();
-		findwords();
-		//cout << word0 << " " << word1 << endl;
-		findletter();
+		cout << path.at(i).source << " " << path.at(i).dest << endl;
 	}
+	cout << "head source=" << gr.returnHead().source<<" head dest="<<gr.returnHead().dest<<endl;
 }
